0be0f61fe55d396730485c5ad912dc24946a9a73
tashabes <natasha_abel@hotmail.co.uk>
2023-06-15 16:00:36 +0100

    Book API Added

--- BookApp.API/Configurations/MapperConfig.cs
+++ BookApp.API/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BookApp.API.Data;
 using BookApp.API.Models.Author;
+using BookApp.API.Models.Book;
 
 namespace BookApp.API.Configurations
 {
@@ -8,9 +9,20 @@ namespace BookApp.API.Configurations
     {
         public MapperConfig()
         {
+            CreateMap<AuthorReadOnlyDto, Author>().ReverseMap();
             CreateMap<AuthorCreateDto, Author>().ReverseMap();
             CreateMap<AuthorUpdateDto, Author>().ReverseMap();
-            CreateMap<AuthorReadOnlyDto, Author>().ReverseMap();
+
+            CreateMap<BookCreateDto, Book>().ReverseMap();
+            CreateMap<BookUpdateDto, Book>().ReverseMap();
+            CreateMap<Book, BookReadOnlyDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+
+            CreateMap<Book, BookDetailsDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+
 
         }
     }
