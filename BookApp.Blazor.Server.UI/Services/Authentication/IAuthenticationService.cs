ae25a8b0a29de3f2d9bc81e9b39dc013cbb2e449
tashabes <natasha_abel@hotmail.co.uk>
2023-06-20 15:41:56 +0100

    Completed setup of Blazor Server App and Authentication

---
+++ BookApp.Blazor.Server.UI/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using BookApp.Blazor.Server.UI.Services.Base;
+
+namespace BookApp.Blazor.Server.UI.Services.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> AuthenticateAsync(LoginUserDto loginModel);
+        public Task Logout();
+    }
+}
